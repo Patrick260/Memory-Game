690d98c8835f179663d60637de8966cba6074dcc
Patrick260 <Patrick260@protonmail.com>
2022-01-03 16:19:23 +0100

    Add basic gui

---
+++ src/de/patrick260/memory/gui/GUI.java
@@ -0,0 +1,16 @@
+package de.patrick260.memory.gui;
+
+import javax.swing.*;
+
+public class GUI extends JFrame {
+
+    public GUI() {
+
+        setTitle("Memory");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+
+    }
+
+}
--- src/de/patrick260/memory/main/Memory.java
+++ src/de/patrick260/memory/main/Memory.java
@@ -1,10 +1,14 @@
 package de.patrick260.memory.main;
 
+import de.patrick260.memory.gui.GUI;
+
+import java.awt.*;
+
 public class Memory {
 
     public static void main(String[] args) {
 
-
+        EventQueue.invokeLater(() -> new GUI().setVisible(true));
 
     }
 
